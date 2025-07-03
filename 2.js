8ae1ccc55fc0ce596d5c6da91741f646b3ee0b5b
RohitPawar2005 <pawarrohit60902@gmail.com>
2025-07-03 18:02:20 +0530

    new

---
+++ 2.js
@@ -0,0 +1,36 @@
+//nested if eles
+// if(condtion){
+//     if(condtion){
+
+//     }
+// }
+
+// let age = paresenInt(import("enter you number"))
+// if (age>=18){
+//     let n = parseInt(import("paress 1 for indian"))
+//     if(n==1){
+//         alert("eligibl")
+//     }
+// }
+// else{
+//     alert("minor...")
+// }
+
+
+
+// let num = 1
+// do{
+//     console.log(num1);
+//     num1++
+// }while(num1<=10);
+
+///  1  writ a program to prent rives tebal ani number
+
+let num = parseInt(prompt("enter ani number"))
+let i = 10;
+
+do{
+    console.log(num * i);
+    i--;
+
+}while(i >=1);
---
+++ hello.js
@@ -0,0 +1,12 @@
+console.log("inside js")
+document.write("<h1> hello </h1>")
+
+
+// varibles ->
+// var -> redeclare, reasing value, function scope
+// let - > not redeclare, reasing value, block scope
+
+// const - > not not block scope
+
+
+// ===============================================================
