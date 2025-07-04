9ceab7235e15dede47774349f9f27d96b10ab27d
RohitPawar2005 <pawarrohit60902@gmail.com>
2025-07-04 17:31:34 +0530

    first commit

--- 2.js
+++ 2.js
@@ -13,24 +13,78 @@
 //     }
 // }
 // else{
-//     alert("minor...")
+// //     alert("minor...")
+// // }
+
+
+
+// // let num = 1
+// // do{
+// //     console.log(num1);
+// //     num1++
+// // }while(num1<=10);
+
+// ///  1  writ a program to prent rives tebal ani number
+
+// let num = parseInt(prompt("enter ani number"))
+// let i = 10;
+
+// do{
+//     console.log(num * i);
+//     i--;
+
+// }while(i >=1);
+
+
+
+// for loop
+ for (let a = 1 ; a<11 ; a++){
+    console.log(a);
+ }
+
+
+
+for (let b = 1 ; b>1 ; b--){
+    console.log(b);
+ }
+
+
+
+//  W A P print qerw of even Number 
+
+// for (let i = 1 ; i<=20 ; i++){
+//      if(i % 2 == 0){
+//         console.log(i*2)
+//      }
+//  }
+
+
+ // nested for loop 
+//   for (let a=2 ; a<=3 ; a++){
+//     for (let b = 1 ; b<11 ; b++){
+//         console.log(a*b);
+//     }
+//   }
+  
+
+//   ######
+
+// for (let a = 1 ; a<=5 ;a++){
+//     document.write("#")
 // }
 
 
+// ****** print 
 
-// let num = 1
-// do{
-//     console.log(num1);
-//     num1++
-// }while(num1<=10);
-
-///  1  writ a program to prent rives tebal ani number
-
-let num = parseInt(prompt("enter ani number"))
-let i = 10;
-
-do{
-    console.log(num * i);
-    i--;
-
-}while(i >=1);
+for (let r=1 ; r<=5 ; r++){
+     for (let c = 1 ; c<=5 ; c++){
+        if(r==1 || r==5 || c==1 || c==5){
+            document.write("*")
+        }
+        else{
+             document.write('&nbsp&nbsp')
+        }
+       
+     }
+     document.write("<br>")
+  }
