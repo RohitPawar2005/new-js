e497f7479716dcd404ca28b069b53749f2277cee
RohitPawar2005 <pawarrohit60902@gmail.com>
2025-07-10 18:19:47 +0530

    function2

---
+++ 10function.js
@@ -0,0 +1,84 @@
+// santecsacuch value argument = functio ko 
+// fun  fun {
+// fast type ke 
+
+// wthe argum+
+//  function fun ()
+//  {
+//     let a =10 
+//     return a+a 
+//  }
+//  let b = fun ()
+//  console.log (b)
+
+
+// function ki value ko two typeof she hendel kra saktete hai 
+// functio ki value  function 
+
+// widaout and return value
+
+
+
+
+
+
+
+
+// // //3 whith argument and return
+// // function fun (num)
+// // {
+// //     return num *num 
+// // }
+// // let b = 10
+// // console.log(fun(b))
+
+
+
+// /// w a p to print sum of two number using function type 3
+// function fun (num1 , num2)
+// {
+//     return num1 +num2
+// }
+// let a = 10
+// let b= 20
+// console.log(fun(a,b))
+
+// wthi argument and no retun 
+
+// w a p to tek  inttige and chek even and odd wthi arrgument and no retun 
+ 
+
+//  let numder = parseInt(import("enter the number"))
+//  function chak (num){
+//     if (num%2 == 0){
+//         console.log("number is even")
+//     }
+//     else{
+//         console.log("number is odd")
+//     }
+//  }
+// chak(numder)
+
+
+
+// //// w a p to print tebal any number using function typ two 
+//  function tebal(num)
+//  {
+//     printTable(int) {
+//     for (int i = 1; i <= 10; i++) {
+//         print(num)
+//     }
+// }
+
+//  }
+//  let b = fun ()
+//  console.log (b)
+
+
+// w a p  two print multipal  betwern 1 to 10
+
+
+
+function fun (i){
+     for (int i = 1; i <= 10; i++)
+}
---
+++ 9function.js
@@ -0,0 +1,89 @@
+// // // bolk of code  set of instructions
+// // // for code reuse 
+
+//user defain    type witha out aruument no retun
+//pri defain function 
+
+
+// // //syntext
+
+
+// // function demo(){
+// //     alert("working ")
+
+// // }
+// // demo()
+
+
+// // function add(){
+// //     a = parseInt(prompt("enter 1 numder"))
+// //     b = parseInt(prompt("enter 2 number"))
+// //     console.log(a+b);
+// // }
+// // add()
+
+// // // w a p  to print array of carcal 
+// // // w a p to find rentest numder btween two numder 
+// // // w a p to print you name 10 time 
+// // // w a p to print sum of all even number betwen 1 to 10 aliment 
+// // // w a p to print kyes of an object object sulde have five  object 
+
+// // //1
+
+// function demo1(){
+//     let = parseInt(import("enter radius"))
+//     Area= 3.14*r*r
+//     console.log(Area)
+// }
+// demo1()
+
+
+// //2 
+
+// function demo2(){
+//     num= parseInt(prompt("enter 1 numder"))
+//     num= parseInt(prompt("enter 2 number"))
+//     if(sum<sum){
+//         console.log(num)
+//     }
+// }
+// demo2()
+
+// //3
+
+// function demo3(){
+//     let = parseInt(import("enter you name "))
+//     for (let  a = 0; a<10; a++){
+//         console.log(demo3)
+//     }
+
+    
+// }
+// demo3()
+
+// //4 
+
+
+// function demo4(){
+    
+// num = [1,2,3,4,5,6,7,8,9,10]
+
+//   for( k==num )
+//     if(k%2!==0){
+//        console.log(k)
+//     }
+// }
+// demo4()
+
+
+
+
+function demo5(){
+    let ob ={
+        name: "rohit",age:20, city:"bpl",contcet:23456, 
+    }
+    for (let k in ob ){
+        console.log(k);
+    }
+}
+demo5()
