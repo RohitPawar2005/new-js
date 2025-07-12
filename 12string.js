975e0cfe64014e76dff7310ca149b49afdd3ecfa
RohitPawar2005 <pawarrohit60902@gmail.com>
2025-07-12 22:08:01 +0530

    object

---
+++ 12string.js
@@ -0,0 +1,30 @@
+// function rohit(){
+//     alert("rohit")
+// }
+
+
+
+// function rohit(){
+//     let str= "this is cybrom string"
+//     let str2= "thi is bhopal "
+//     let str3 = "bhopl "
+
+
+//     console.log(str.slice(7,13));// slice 
+//     console.log(str.concat(str2 ,str3)); // concat
+    
+// }
+
+
+// function rohit(){
+
+//     let str= "this is python "
+//     let tri
+// }
+
+function rohit(){
+    let text = " apple mengo banana "
+    console.log(text.split("-"))
+}
+
+// ["element1,element2"]
